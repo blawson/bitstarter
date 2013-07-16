203882eb7fb93e6ddc81a64a957dbd1193016fca
Ben Lawson <blawson@gmail.com>
2013-07-16 07:21:34 +0000

    Updated grader.js to take a URL

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -31,7 +32,7 @@ var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1);
     }
     return instr;
 };
@@ -44,8 +45,8 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(htmlToCheck, checksfile) {
+    $ = htmlToCheck;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -53,6 +54,25 @@ var checkHtmlFile = function(htmlfile, checksfile) {
         out[checks[ii]] = present;
     }
     return out;
+};
+
+var logJson = function(checkJson) {
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
+var checkHtmlFile = function(htmlFile, checksFile) {
+    var file = cheerioHtmlFile(htmlFile);
+    var outJson = checkHtml(file, checksFile);
+    logJson(outJson);
+};
+
+var checkURL = function(url, checksfile) {
+    rest.get(url).on('complete', function(data, response) {
+	    var htmlData = cheerio.load(data);
+        var outJson = checkHtml(htmlData, checksfile);
+        logJson(outJson);
+    });
 };
 
 var clone = function(fn) {
@@ -65,10 +85,13 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to URL to parse')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var functionToUse = (program.url) ? checkURL : checkHtmlFile;
+    var sourceToUse = (program.url) ? program.url : program.file;
+    functionToUse(sourceToUse, program.checks);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
